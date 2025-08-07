5dc3ac0f9dca1cba885556b01bec9b66ee0c18a5
sudhar31 <shansudhar209@gmail.com>
2025-08-07 10:02:41 +0530

    Initial Project Setup with Spring Boot Backend

---
+++ RateMyPark/src/main/java/com/smartpark/ratemypark/RateMyParkApplication.java
@@ -0,0 +1,13 @@
+package com.smartpark.ratemypark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateMyParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateMyParkApplication.class, args);
+	}
+
+}
---
+++ RateMyPark/src/test/java/com/smartpark/ratemypark/RateMyParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartpark.ratemypark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateMyParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
